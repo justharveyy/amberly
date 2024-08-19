4f7b19420ca472f233e2dba43dc4c6598ff19e06
Bui Bao Hoang <hoang.nicolas2409@gmail.com>
2024-08-20 02:10:46 +0700

    yes

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className + " dark"}>
+        <div className="hidden">i love u duong</div>
         {children}
         <Toaster
           toastOptions={{
